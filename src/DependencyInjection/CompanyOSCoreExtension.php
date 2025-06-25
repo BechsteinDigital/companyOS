ecaf67d26d3233aca3ce6e8ccd309d5c9d16ec0d
David Bechstein <david@bechstein-digital.de>
2025-06-26 00:33:40 +0200

    bugfixing, changing folder structure

--- src/DependencyInjection/CompanyOSCoreExtension.php
+++ src/DependencyInjection/CompanyOSCoreExtension.php
@@ -11,7 +11,7 @@ class CompanyOSCoreExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../Resources/config'));
         
         // Services laden
         $loader->load('services.yaml');
